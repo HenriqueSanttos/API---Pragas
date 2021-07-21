b0c669ffd7252c2b6fa8db974483a3bbf3d6c012
Henrique <80371192+SdRHenrique@users.noreply.github.com>
2021-07-21 16:21:56 -0400

    Delete api_pragas - Henrique directory

--- api_pragas - Henrique/src/main/java/com/api_pragas/repository/ISafraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api_pragas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api_pragas.domain.Safra;
-
-public interface ISafraRepository extends JpaRepository<Safra, Long> {
-
-}
