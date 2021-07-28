ad13e688e7c3333e52a621df1e116bd71ec39520
Henrique <80371192+SdRHenrique@users.noreply.github.com>
2021-07-28 10:58:36 -0400

    Delete api_pragas directory

--- api_pragas/src/main/java/com/api_pragas/repository/IVerificacoesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api_pragas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api_pragas.domain.Verificacoes;
-
-public interface IVerificacoesRepository extends JpaRepository<Verificacoes, Long> {
-	
-}
